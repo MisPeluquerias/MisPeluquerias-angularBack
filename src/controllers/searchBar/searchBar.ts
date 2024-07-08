ad095a26fe15e699acaf03bf600c3d20c89790bb
Antonio Zafra <javier@mispeluquerias.com>
2024-07-08 18:37:23 +0200

    end commit

--- src/controllers/searchBar/searchBar.ts
+++ src/controllers/searchBar/searchBar.ts
@@ -1,16 +1,17 @@
 import express from "express";
 const router = express.Router();
 import connection from "../../db/db";
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 
-
-router.get('/searchCategory', async (req, res) => {
+router.get("/searchCategory", async (req, res) => {
   try {
     const { name } = req.query;
 
     if (!name) {
-      return res.status(400).json({ error: "El parámetro 'name' es requerido." });
+      return res
+        .status(400)
+        .json({ error: "El parámetro 'name' es requerido." });
     }
 
     // Iniciar la transacción
@@ -48,8 +49,142 @@ router.get('/searchCategory', async (req, res) => {
   }
 });
 
+router.get("/searchService", async (req, res) => {
+  try {
+    const { name } = req.query;
 
-router.get('/searchServiceOrSalon', async (req, res) => {
+    if (!name) {
+      return res
+        .status(400)
+        .json({ error: "El parámetro 'name' es requerido." });
+    }
+
+    // Iniciar la transacción
+    await new Promise((resolve, reject) => {
+      connection.beginTransaction((err) => {
+        if (err) return reject(err);
+        resolve(undefined);
+      });
+    });
+
+    const query = "SELECT * FROM service WHERE name LIKE ?";
+
+    connection.query(query, [`%${name}%`], (error, results) => {
+      if (error) {
+        console.error("Error al buscar el servicio:", error);
+        return connection.rollback(() => {
+          res.status(500).json({ error: "Error al buscar el servicio." });
+        });
+      }
+
+      connection.commit((err) => {
+        if (err) {
+          console.error("Error al hacer commit:", err);
+          return connection.rollback(() => {
+            res.status(500).json({ error: "Error al buscar el servicio." });
+          });
+        }
+
+        res.json(results);
+      });
+    });
+  } catch (err) {
+    console.error("Error al buscar el servicio:", err);
+    res.status(500).json({ error: "Error al buscar el servicio." });
+  }
+});
+
+router.get("/searchSalon", async (req, res) => {
+  try {
+    const { name } = req.query;
+
+    if (!name) {
+      return res
+        .status(400)
+        .json({ error: "El parámetro 'name' es requerido." });
+    }
+
+    // Iniciar la transacción
+    await new Promise((resolve, reject) => {
+      connection.beginTransaction((err) => {
+        if (err) return reject(err);
+        resolve(undefined);
+      });
+    });
+
+    const query = "SELECT * FROM salon WHERE name LIKE ?";
+
+    connection.query(query, [`%${name}%`], (error, results) => {
+      if (error) {
+        console.error("Error al buscar el salón:", error);
+        return connection.rollback(() => {
+          res.status(500).json({ error: "Error al buscar el salón." });
+        });
+      }
+
+      connection.commit((err) => {
+        if (err) {
+          console.error("Error al hacer commit:", err);
+          return connection.rollback(() => {
+            res.status(500).json({ error: "Error al buscar el salón." });
+          });
+        }
+
+        res.json(results);
+      });
+    });
+  } catch (err) {
+    console.error("Error al buscar el salón:", err);
+    res.status(500).json({ error: "Error al buscar el salón." });
+  }
+});
+
+router.get("/searchSalon", async (req, res) => {
+  try {
+    const { name } = req.query;
+
+    if (!name) {
+      return res
+        .status(400)
+        .json({ error: "El parámetro 'name' es requerido." });
+    }
+
+    // Iniciar la transacción
+    await new Promise((resolve, reject) => {
+      connection.beginTransaction((err) => {
+        if (err) return reject(err);
+        resolve(undefined);
+      });
+    });
+
+    const query = "SELECT * FROM salon WHERE name LIKE ?";
+
+    connection.query(query, [`%${name}%`], (error, results) => {
+      if (error) {
+        console.error("Error al buscar el salón:", error);
+        return connection.rollback(() => {
+          res.status(500).json({ error: "Error al buscar el salón." });
+        });
+      }
+
+      connection.commit((err) => {
+        if (err) {
+          console.error("Error al hacer commit:", err);
+          return connection.rollback(() => {
+            res.status(500).json({ error: "Error al buscar el salón." });
+          });
+        }
+
+        res.json(results);
+      });
+    });
+  } catch (err) {
+    console.error("Error al buscar el salón:", err);
+    res.status(500).json({ error: "Error al buscar el salón." });
+  }
+});
+
+router.get("/searchCity", async (req, res) => {
   try {
     const { name } = req.query;
 
@@ -65,13 +200,19 @@ router.get('/searchServiceOrSalon', async (req, res) => {
       });
     });
 
-    const query = "SELECT * FROM category WHERE name LIKE ? OR service LIKE ? OR salon LIKE ?";
     
-    connection.query(query, [`%${name}%`], (error, results) => {
+    const query = `
+      SELECT name, MIN(zip_code) AS zip_code
+      FROM city
+      WHERE name LIKE ? OR zip_code LIKE ?
+      GROUP BY name
+    `;
+
+    connection.query(query, [`%${name}%`, `%${name}%`], (error, results) => {
       if (error) {
-        console.error("Error al buscar el servicio o salon:", error);
+        console.error("Error al buscar la ciudad:", error);
         return connection.rollback(() => {
-          res.status(500).json({ error: "Error al buscar la categoría." });
+          res.status(500).json({ error: "Error al buscar la ciudad." });
         });
       }
 
@@ -79,7 +220,7 @@ router.get('/searchServiceOrSalon', async (req, res) => {
         if (err) {
           console.error("Error al hacer commit:", err);
           return connection.rollback(() => {
-            res.status(500).json({ error: "Error al buscar el servicio o salon." });
+            res.status(500).json({ error: "Error al buscar la ciudad." });
           });
         }
 
@@ -87,11 +228,10 @@ router.get('/searchServiceOrSalon', async (req, res) => {
       });
     });
   } catch (err) {
-    console.error("Error al buscar el servicio o salon:", err);
-    res.status(500).json({ error: "Error al buscar el servicio o salon." });
+    console.error("Error al buscar la ciudad:", err);
+    res.status(500).json({ error: "Error al buscar la ciudad." });
   }
 });
-
 
 
 export default router;
